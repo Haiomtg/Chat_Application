bde8ad634a4d606b05c3d88fa8117e277940f5b1
[Haiomtg] <[haiomtg@gmail.com]>
2024-10-13 21:43:31 +1100

    first change

---
+++ chat-application/src/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // ... existing configuration ...
+    parserOptions: {
+      // ... existing parser options ...
+      requireConfigFile: false, // Add this line
+    },
+  };
